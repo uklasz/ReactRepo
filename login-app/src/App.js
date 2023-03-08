29f8bb560b6b63e9df46c2ceb1c2a2f2e9ec2d75
Lukasz Piatek <uklaszp@gmail.com>
2023-03-08 14:12:15 +0100

    step7-addProtectedRoute

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Outlet, Navigate } from 'react-router-dom';
+import { getToken } from './utils/session'
 import HomePage from './components/HomePage';
 import ErrorPage from './components/ErrorPage';
 import LoginPage from './components/LoginPage';
-import Unauthorize from './components/Unauthorize';
+
 
 function App() {
   return (
@@ -11,8 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Outlet />}>
           <Route path="" exac element={<LoginPage />} />
-          <Route path="HomePage" element={<HomePage />} />
-          <Route path="Unauthorize" element={<Unauthorize />} />
+          <Route exact path="HomePage" element={getToken() ? <HomePage /> : <Navigate to='/' replace />}/>
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
